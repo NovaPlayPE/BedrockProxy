ff2d67142f8289fd09d08d27a726e46f3a30e92e
GamakCZ <gamak.mcpe@gmail.com>
2020-07-23 23:13:53 +0200

    Add start scripts

---
+++ start.cmd
@@ -0,0 +1,4 @@
+@echo off
+TITLE BedrockProxy server software for Minecraft: Pocket Edition;
+
+java -jar BedrockProxy-1.0-SNAPSHOT.jar
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+java -jar BedrockProxy-1.0-SNAPSHOT.jar
